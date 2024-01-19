3e43505fca0c0a96dd6ca2c8913c480091ca6fc1
Amaury Semery <66590281+AmaurySemery@users.noreply.github.com>
2024-01-19 01:19:30 +0100

    Simple compteur sans Vuex

---
+++ vuex_counter/src/components/Counter.vue
@@ -0,0 +1,36 @@
+// snippet vbase
+
+<template>
+    <div>
+        <h2>{{ currentValue }}</h2>
+        <div>
+            <button @click="increment">+</button>
+            <button @click="decrement">-</button>
+        </div>
+    </div>
+    
+</template>
+
+<script>
+// snippet vdata
+    export default {
+        data() {
+            return {
+                currentValue: 0
+            }
+        },
+// snippet vmethod
+        methods: {
+            increment() {
+                this.currentValue = this.currentValue +1;
+            },
+            decrement() {
+                this.currentValue = this.currentValue -1;
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
